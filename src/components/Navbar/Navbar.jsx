79b12a475cad8c161ca5000972e287b945a03f8c
unknown <drjerry000@gmail.com>
2021-12-09 11:02:20 +0100

    just commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,49 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Link, Button, Badge, Avatar, IconButton, useMediaQuery } from '@material-ui/core';
+import { Notifications, Bookmark } from '@material-ui/icons';
 import React from 'react';
 import useStyles from './style';
+import logo from '../../assets/images/void.png';
 
 const Navbar = () => {
 
     const classes = useStyles();
+    const isAuthPage = false;
+    const isAuthenticated = true;
+    const isMobile = useMediaQuery('(max-width: 900px)');
 
     return (
-        <AppBar position="static">
+        <AppBar position="fixed" className={classes.appbar}>
             <Toolbar className={classes.toolbar}>
-                <Typography variant="h1"></Typography>
+                <div className={classes.logoWrapper}>
+                    <img src={logo} className={classes.logo} alt="logo_void" />
+                    {!isAuthPage && !isMobile && (
+                        <>
+                            <Button variant="contained" className={classes.navigation} href="#contained-buttons">Home</Button>
+                            <Button variant="outlined" className={classes.navigation} href="#contained-buttons">Blog</Button>
+                            <Button variant="outlined" className={classes.navigation} href="#contained-buttons">Contact</Button>
+                            <Button variant="outlined" className={classes.navigation} href="#contained-buttons">Support</Button>    
+                        </>
+                    )}
+                </div>
+                <div className={classes.menus}>
+                {!isAuthPage && !isAuthenticated && (
+                    <>
+                        <Button variant="contained" className={classes.signup} href="#contained-buttons">Sign Up</Button>
+                        <Button variant="outlined" className={classes.signin} href="#contained-buttons">Sign In</Button>    
+                    </>
+                )}
+                {!isAuthPage && isAuthenticated && (
+                    <>
+                        <Badge variant="dot" color="secondary" className={classes.badge}>
+                            <Notifications className={classes.icon} />
+                        </Badge>
+                        <Badge badgeContent={5} color="secondary" className={classes.badge}>
+                            <Bookmark className={classes.icon} />
+                        </Badge>
+                        <Avatar src="/broken-image.jpg" style={{ width: 30, height: 30 }} />
+                    </>
+                )}
+                </div>
             </Toolbar>
         </AppBar>
     )
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,8 +1,49 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, alpha } from '@material-ui/core';
 
+const colour = '#040459';
 
 export default makeStyles(theme=> ({
-    navbar: {
-        
+    appbar: {
+        backgroundColor: colour,
+        height: 80,
+    },
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    logoWrapper: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    navigation: {
+        color: theme.palette.common.white,
+        backgroundColor: colour,
+        boxShadow: 'none',
+        border: 'none',
+        "&:hover": {
+            backgroundColor: alpha(theme.palette.common.white, 0.3),
+        }
+    },
+    logo: {
+        height: 50,
+        color: theme.palette.common.white
+    },
+    signup: {
+        color: colour,
+    },
+    signin: {
+        color: theme.palette.common.white
+    },
+    menus: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    icon: {
+        fontSize: 22,
+    },
+    badge: {
+        marginRight: 20,
     }
 }))
